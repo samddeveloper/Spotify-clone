4fdb1f310c1e3d4ef17791979fd944e75fd4154b
sammie slootman <sammie.slootman@gmail.com>
2024-08-02 15:23:04 +0200

    added MUI, added a colorpalette and updated env variables

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,16 @@
 import './App.css';
+import Stack from '@mui/material/Stack';
+import Button from '@mui/material/Button';
 
 function App() {
 	return (
 		<div className="App">
 			<h1>Techover Self Made - Spotify</h1>
+			<Stack spacing={2} direction="row">
+				<Button variant="text">Text</Button>
+				<Button variant="contained">Contained</Button>
+				<Button variant="outlined">Outlined</Button>
+			</Stack>
 		</div>
 	);
 }
