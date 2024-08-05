2554176b913e8dcc0bd8b61a9a0d72eea0fe462c
sammie slootman <sammie.slootman@gmail.com>
2024-08-05 15:47:22 +0200

    Made Routing, Access token experiance and conditional rendering for if token exists

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,34 @@
 import './App.css';
 import Box from '@mui/material/Box';
+import Dashboard from './components/dashboard/Dashboard';
 import Login from './pages/Login';
-import Home from './pages/Home';
+import { getAccessToken } from './utils/getAccessToken';
+import { getAccessTokenFromStorage } from './utils/getAccessTokenFromStorage';
+import { useEffect, useState } from 'react';
+import { Routes, Route } from 'react-router-dom';
 
 function App({ spotifyApi }) {
-	console.log(spotifyApi);
+	const [token, setToken] = useState(getAccessTokenFromStorage());
+
+	useEffect(() => {
+		const accessToken = getAccessTokenFromStorage() || getAccessToken();
+
+		if (accessToken) {
+			setToken(accessToken);
+			sessionStorage.setItem('spotifyToken', accessToken);
+			window.location.hash = '';
+		}
+	}, []);
+
 	return (
 		<Box className="App">
-			<Home />
+			{token ? (
+				<Dashboard spotifyApi={spotifyApi} />
+			) : (
+				<Routes>
+					<Route path="*" element={<Login />} />
+				</Routes>
+			)}
 		</Box>
 	);
 }
