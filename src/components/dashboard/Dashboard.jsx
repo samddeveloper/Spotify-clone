41cf5e1e55d03ff85f9103cc08f792776e4f91b3
sammie slootman <sammie.slootman@gmail.com>
2024-08-10 00:49:53 +0200

    made playlist component with first Box

--- src/components/dashboard/Dashboard.jsx
+++ src/components/dashboard/Dashboard.jsx
@@ -4,6 +4,7 @@ import Home from '../../pages/Home';
 import SideNav from '../sidenav/SideNav';
 import { getAccessTokenFromStorage } from '../../utils/getAccessTokenFromStorage';
 import { useEffect, useState } from 'react';
+import Playlist from '../../pages/Playlist';
 
 const Dashboard = ({ spotifyApi }) => {
 	const [token, setToken] = useState(getAccessTokenFromStorage());
@@ -21,7 +22,7 @@ const Dashboard = ({ spotifyApi }) => {
 			<Box sx={{ flex: 1, overflowY: 'auto', display: 'flex' }}>
 				<SideNav spotifyApi={spotifyApi} token={null} />
 				<Routes>
-					<Route path="playlist/:id" element={<div>Playlist</div>} />
+					<Route path="playlist/:id" element={<Playlist spotifyApi={spotifyApi} token={null} />} />
 					<Route path="/libary" element={<div>div</div>} />
 					<Route path="/" element={<Home />} />
 				</Routes>
---
+++ src/pages/Playlist.jsx
@@ -0,0 +1,11 @@
+import { Box } from '@mui/material';
+
+const Playlist = ({ spotifyApi, token }) => {
+	return (
+		<Box id="Playlist__page" sx={{ backgroundColor: 'background.paper', flex: 1, overflowY: 'auto' }}>
+			<Box></Box>
+		</Box>
+	);
+};
+
+export default Playlist;
