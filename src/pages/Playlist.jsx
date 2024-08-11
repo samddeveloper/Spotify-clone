cc0eefdc1d9b90f37187c6569c5a0303f89e8726
sammie slootman <sammie.slootman@gmail.com>
2024-08-11 21:27:44 +0200

    get tracks data

--- src/pages/Playlist.jsx
+++ src/pages/Playlist.jsx
@@ -1,6 +1,45 @@
 import { Avatar, Box, Typography } from '@mui/material';
+import { useEffect, useState } from 'react';
+import { useParams } from 'react-router-dom';
 
 const Playlist = ({ spotifyApi, token }) => {
+	const [playlistInfo, setPlaylistInfo] = useState({});
+	const [songs, setSongs] = useState();
+	const { id } = useParams();
+
+	useEffect(() => {
+		const getData = async () => {
+			try {
+				const playlistDetails = await spotifyApi.getPlaylist(id);
+				const images = playlistDetails.body.images;
+
+				const defaultImageUrl = 'https://via.placeholder.com/300'; // Fallback image URL
+				let imageUrl = defaultImageUrl;
+
+				if (images && images.length > 0) {
+					const validImages = images.filter((img) => img.url);
+					if (validImages.length > 0) {
+						imageUrl = validImages[0].url;
+					}
+				}
+
+				setPlaylistInfo({
+					image: imageUrl,
+					name: playlistDetails.body.name
+				});
+				console.log(playlistDetails);
+				const { items } = playlistDetails.body.tracks;
+				//format songs
+				setSongs(items);
+				console.log(items);
+			} catch (e) {
+				console.error(e);
+			}
+		};
+
+		getData();
+	}, [id, spotifyApi]);
+
 	return (
 		<Box id="Playlist__page" sx={{ backgroundColor: 'background.paper', flex: 1, overflowY: 'auto' }}>
 			<Box
@@ -17,15 +56,15 @@ const Playlist = ({ spotifyApi, token }) => {
 				}}
 			>
 				<Avatar
-					src={null}
+					src={playlistInfo?.image}
 					variant="square"
-					alt={null}
+					alt={playlistInfo?.name}
 					sx={{ boxShadow: 15, width: { xs: '100%', md: 235 }, height: { xs: '100%', md: 235 } }}
 				/>
 				<Box>
 					<Typography sx={{ fontSize: 12, fontWeight: 'bold', color: 'text.primary' }}>Playlist</Typography>
 					<Typography sx={{ fontSize: { xs: 42, md: 72 }, fontWeight: 'bold', color: 'text.primary' }}>
-						Fake playlist name
+						{playlistInfo?.name}
 					</Typography>
 				</Box>
 			</Box>
